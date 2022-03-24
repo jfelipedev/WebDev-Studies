0edd9b74d57be1b8341b153d978f47d6313d5bd9
Felipe <joandersonfsbarbosa@gmail.com>
2022-03-24 17:40:31 -0300

    Relógio HTML/CSS/JS

---
+++ DayNite JS Watch/script.js
@@ -0,0 +1,26 @@
+function carregar(){
+var msg = document.querySelector('div.msg');
+var img = document.querySelector('img.imagem');
+var data = new Date();
+var hora = data.getHours()
+var minuto = data.getMinutes()
+
+
+if (hora >= 5 && hora < 12){
+  img.src = 'imgs/morningweb.png';
+  msg.innerHTML = `Bom dia! Agora são ${hora}:${minuto} horas da manhã.`
+  document.body.style.background = '#94840e'
+}else if (hora >= 12 && hora < 16){
+  img.src = 'imgs/afternoonweb.png';
+  msg.innerHTML = `Boa tarde! Agora são ${hora}:${minuto} horas.`
+  document.body.style.background = '#cde4fe'
+}else if (hora >= 16 && hora <= 18) {
+  img.src = 'imgs/sunsetweb.png'
+  msg.innerHTML = `Boa tarde! Agora são ${hora}:${minuto} horas..`
+  document.body.style.background = '#ff8400'
+} else {
+  img.src = 'imgs/nightweb.png'
+  msg.innerHTML = `Boa noite! Agora são ${hora}:${minuto} horas..`
+  document.body.style.background = '#0e2b2d'
+}
+}
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # WebDev-Studies
 
 Repositório feito para realizar commits dos resultados dos exercícios que pratico para desenvolver minhas habilidades em HTML, CSS e JavaScript
-
-bla
