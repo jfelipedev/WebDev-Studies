5254113627e21f5376f88af3ff535651745a658b
Joanderson Felipe <86390899+jfelipedev@users.noreply.github.com>
2022-04-01 15:15:34 -0300

    Update tasks.js

--- LoginSignup/scripts/tasks.js
+++ LoginSignup/scripts/tasks.js
@@ -7,7 +7,7 @@ let userLogged = JSON.parse(localStorage.getItem('userLogged'))
 userName.innerHTML = `${userLogged.name}`
 
 // This IF prevents the user from entering the app's page without log in
-if (localStorage.getItem(token) == null) {
+if (localStorage.getItem('token') == null) {
   alert('Você precisa estar logado para acessar essa página!')
   window.location.href = 'https://jfelipedev.github.io/WebDev-Studies/LoginSignup/index.html
 '
